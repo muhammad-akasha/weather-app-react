99ac4062c557669ee463d3cc16e2da012f1ef6bc
Muhammad Akasha <153507162+muhammad-akasha@users.noreply.github.com>
2024-10-03 15:17:28 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Creating weather app using react js
+Creating Weather App using react js.
+
+In this repository user can get weather update of any city.
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Swal from "sweetalert2";
 
 function App() {
   const [weatherArr, setWeatherArr] = useState([]);
@@ -38,33 +39,51 @@ function App() {
       return <i className="text-[150px] fa-solid fa-cloud"></i>;
     }
   }
+
   function getWeather(e) {
     e.preventDefault();
     const currCity = city.current.value;
+    if (!currCity) {
+      Swal.fire({
+        icon: "error",
+        title: "Please Enter Any City",
+        position: "top",
+      });
+      return;
+    }
     if (cities.includes(currCity)) {
-      alert("city already search");
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "City Already Searched",
+        position: "top",
+      });
       return;
     }
     cities.push(currCity);
     setCIties([...cities]);
     axios(
-      `https://api.weatherapi.com/v1/current.json?key=${appId}&q=${currCity}&aqi=yes`
+      `https://api.weatherapi.com/v1/forecast.json?key=${appId}&q=${currCity}&aqi=yes&days=7`
     )
       .then((res) => {
         weatherArr.unshift(res.data);
         setWeatherArr([...weatherArr]);
       })
       .catch((err) => {
-        alert("City Not Correct Please Enter Correct City");
-        console.log(err);
+        alert(err.response.data.error.message);
       });
     city.current.value = "";
   }
 
   return (
     <div>
-      <div className="navbar bg-teal-700 py-5">
-        <a className="btn btn-ghost text-3xl text-slate-300 font-semibold">
+      <div
+        className="navbar text-center py-5"
+        style={{
+          background: "linear-gradient(0deg, #3E2D8F 0%, #9D52AC 100%)",
+        }}
+      >
+        <a className="btn btn-ghost m-auto text-3xl text-slate-300 font-semibold">
           Weather App
         </a>
       </div>
@@ -77,7 +96,7 @@ function App() {
       >
         <h1
           style={{ letterSpacing: "2px" }}
-          className="text-center font-semibold text-[33px]"
+          className="text-center text-slate-300 font-semibold text-[33px]"
         >
           Welcome to the weather app
         </h1>
@@ -105,11 +124,16 @@ function App() {
                   wind_kph,
                   condition: { icon, text },
                 },
+                forecast: { forecastday },
               } = weather;
               return (
                 <div
                   key={index}
-                  className="mb-4 bg-slate-300 w-[700px] px-[20px] py-[40px] my-[20px] rounded-md mx-auto"
+                  className="mb-4 text-[#fff] w-[100%] md:w-[700px] lg:w-[800px] px-[20px] py-[40px] my-[20px] rounded-md mx-auto"
+                  style={{
+                    background:
+                      "linear-gradient(0deg, #3E2D8F 0%, #9D52AC 100%)",
+                  }}
                 >
                   <h2
                     style={{ letterSpacing: "5px" }}
@@ -117,22 +141,56 @@ function App() {
                   >
                     {setTimePeriod(localtime)}
                   </h2>
-                  <div className="flex justify-around mt-[20px]">
+                  <div className="flex flex-col-reverse text-center md:justify-around sm:text-left sm:flex-row mt-[20px]">
                     <div>
                       <h2>
                         {name}, {country}
                       </h2>
-                      <h2> Temperture : {temp_c}°C</h2>
-                      <h2>Feel like : {feelslike_c}°C</h2>
+                      <h2>Temperature: {temp_c}°C</h2>
+                      <h2>Feels Like: {feelslike_c}°C</h2>
                       <h2>
                         Wind Speed {wind_kph}{" "}
                         <i className="fa-solid fa-wind"></i> km/h
                       </h2>
-                      <h2 className="flex gap-2 items-center">
+                      <h2 className="flex justify-center sm:justify-start md:gap-2 md:items-center">
                         {text} <img src={icon} alt={text} />
                       </h2>
                     </div>
                     <div>{manageWeatherIcon(text)}</div>
+                  </div>
+                  <div className="mt-4">
+                    <h3 className="text-center text-[24px]">7-Day Forecast</h3>
+                    <div className="flex gap-3 justify-center text-center flex-wrap">
+                      {forecastday.map((forecast, dayIndex) => {
+                        const {
+                          date,
+                          day: {
+                            condition: { text: foreText, icon: foreIcon },
+                            mintemp_c,
+                            maxtemp_c,
+                          },
+                        } = forecast;
+                        return (
+                          <div
+                            key={dayIndex}
+                            className="text-center mt-4 text-[14px] shadow-lg p-[5px]"
+                          >
+                            <h6>
+                              {" "}
+                              <span>Date</span> {date.slice(8)}
+                            </h6>
+                            <img
+                              className="m-auto"
+                              src={foreIcon}
+                              alt={foreText}
+                            />
+                            <h6>{foreText}</h6>
+                            <h6>Min:{mintemp_c}°C</h6>
+                            <h6>Max: {maxtemp_c}°C</h6>
+                          </div>
+                        );
+                      })}
+                    </div>
                   </div>
                 </div>
               );
